145be3dcd70ddd8c682e6be06529c6fe002aecb1
oriionn <contact@oriondev.fr>
2024-02-18 01:09:37 +0100

    Ajout du controle de la musique

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 var music = document.querySelector('#music-play');
 var musicContainer = document.querySelector('#music');
 var container = document.querySelector('#container');
+var musicControl = document.querySelector('#music-control');
 musicContainer.onclick = function () {
     music.play();
     music.loop = true;
     music.volume = 0.5;
     musicContainer.style.display = 'none';
     container.style.opacity = '1';
+    musicControl.style.opacity = '1';
+    musicControl.onclick = function () {
+        if (music.paused) {
+            music.play();
+            musicControl.innerHTML = '<i data-feather="volume-2"></i>';
+        }
+        else {
+            music.pause();
+            musicControl.innerHTML = '<i data-feather="volume-x"></i>';
+        }
+        // @ts-ignore
+        feather.replace();
+    };
 };
+// @ts-ignore
+feather.replace();
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 const music: HTMLAudioElement = document.querySelector('#music-play');
 const musicContainer: HTMLDivElement = document.querySelector('#music');
 const container: HTMLDivElement = document.querySelector('#container');
+const musicControl: HTMLDivElement = document.querySelector('#music-control');
 
 musicContainer.onclick = () => {
   music.play();
@@ -8,4 +9,20 @@ musicContainer.onclick = () => {
   music.volume = 0.5;
   musicContainer.style.display = 'none';
   container.style.opacity = '1';
+  musicControl.style.opacity = '1';
+
+  musicControl.onclick = () => {
+    if (music.paused) {
+      music.play();
+      musicControl.innerHTML = '<i data-feather="volume-2"></i>';
+    } else {
+      music.pause();
+      musicControl.innerHTML = '<i data-feather="volume-x"></i>';
+    }
+    // @ts-ignore
+    feather.replace();
+  }
 }
+
+// @ts-ignore
+feather.replace();
